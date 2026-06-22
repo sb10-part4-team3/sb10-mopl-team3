2a91687cc83fcc9d171aaeff4c88b08bded28a26
김현민 <rlagusals911@naver.com>
2026-06-22 09:39:25 +0900

    chore: 프로젝트 초기 설정

---
+++ src/test/java/com/example/sb10_MoPl_team3/Sb10MoPlTeam3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb10_MoPl_team3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb10MoPlTeam3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
