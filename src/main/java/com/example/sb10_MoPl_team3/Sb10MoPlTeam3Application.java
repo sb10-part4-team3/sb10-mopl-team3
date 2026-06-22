8f9aa25d720848b9cdc30793a0604085b7f36a8d
김현민 <rlagusals911@naver.com>
2026-06-22 09:39:25 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/example/sb10_MoPl_team3/Sb10MoPlTeam3Application.java
@@ -0,0 +1,13 @@
+package com.example.sb10_MoPl_team3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb10MoPlTeam3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb10MoPlTeam3Application.class, args);
+	}
+
+}
