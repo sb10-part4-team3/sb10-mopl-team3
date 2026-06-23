f004e2bb76eb747392f8fa17959b7d975be4c3d3
rorm0819 <rorm0819@gmail.com>
2026-06-23 15:16:06 +0900

    fix: 리뷰 피드백 반영

--- src/main/java/com/example/sb10_MoPl_team3/playlist/dto/PlaylistDto.java
+++ src/main/java/com/example/sb10_MoPl_team3/playlist/dto/PlaylistDto.java
@@ -1,6 +1,6 @@
 package com.example.sb10_MoPl_team3.playlist.dto;
 
-import com.example.sb10_MoPl_team3.content.dto.ContentDto;
+import com.example.sb10_MoPl_team3.content.dto.ContentSummary;
 import com.example.sb10_MoPl_team3.domain.user.dto.response.UserSummaryResponse;
 
 import java.time.Instant;
@@ -9,13 +9,12 @@ import java.util.UUID;
 
 public record PlaylistDto(
         UUID id,
-        // 후에 UserSummery로 변경 필요
         UserSummaryResponse owner,
         String title,
         String description,
         Instant updatedAt,
         Long subscriberCount,
         boolean subscribedByMe,
-        List<ContentDto> contents
+        List<ContentSummary> contents
 ) {
 }
