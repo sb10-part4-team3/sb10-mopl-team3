96e7b99b7e2d343e0a507e16e447784f7cd5dcab
안승리 <hauall@naver.com>
2026-06-23 17:49:32 +0900

    feat: 콘텐츠 create 구현

--- src/main/java/com/example/sb10_MoPl_team3/content/service/ContentServiceImpl.java
+++ src/main/java/com/example/sb10_MoPl_team3/content/service/ContentServiceImpl.java
@@ -4,21 +4,55 @@ import com.example.sb10_MoPl_team3.content.dto.ContentCreateRequest;
 import com.example.sb10_MoPl_team3.content.dto.ContentDto;
 import com.example.sb10_MoPl_team3.content.entity.Content;
 import com.example.sb10_MoPl_team3.content.repository.ContentRepository;
+import com.example.sb10_MoPl_team3.global.file.FileStorageService;
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class ContentServiceImpl implements ContentService{
+public class ContentServiceImpl implements ContentService {
 
   private final ContentRepository contentRepository;
+  private final FileStorageService fileStorageService;
 
   @Override
   @Transactional
-  public ContentDto create(ContentCreateRequest request) {
-    return null;
+  public ContentDto create(ContentCreateRequest request, MultipartFile thumbnail) {
+
+    String thumbnailUrl = null;
+    if (thumbnail != null && !thumbnail.isEmpty()) {
+      thumbnailUrl = fileStorageService.upload(thumbnail);
+    }
+
+    List<String> tags = request.tags() != null ? request.tags() : List.of(); // TODO 태그엔티티 생성후 수정 필요
+
+    Content content = Content.builder()
+        .type(request.type())
+        .title(request.title())
+        .description(request.description())
+        .thumbnailUrl(thumbnailUrl)
+        .externalId(UUID.randomUUID().toString()) // TODO 외부 API 연동시 수정 필요
+        .source("MANUAL")  // // TODO 외부 API 연동시 수정 필요
+        .build();
+
+    Content savedContent = contentRepository.save(content);
+
+    return new ContentDto(
+        savedContent.getId(),
+        savedContent.getType(),
+        savedContent.getTitle(),
+        savedContent.getDescription(),
+        savedContent.getThumbnailUrl(),
+        tags, // TODO 태그엔티티 생성후 수정 필요
+        0.0,
+        0,
+        0L
+    );
   }
 }
