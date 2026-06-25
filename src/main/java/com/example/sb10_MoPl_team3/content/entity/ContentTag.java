1bc2247b3cf9ecd8d837ee27adcef3ec0567b1d0
안승리 <hauall@naver.com>
2026-06-25 10:01:50 +0900

    feat: 콘텐츠태그 엔티티 및 레포지토리 구현

---
+++ src/main/java/com/example/sb10_MoPl_team3/content/entity/ContentTag.java
@@ -0,0 +1,40 @@
+package com.example.sb10_MoPl_team3.content.entity;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "content_tags")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = "id")
+public class ContentTag {
+
+  @EmbeddedId
+  private ContentTagId id;
+
+  @MapsId("contentId")
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "content_id")
+  private Content content;
+
+  @MapsId("tagId")
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "tag_id")
+  private Tag tag;
+
+  public ContentTag(Content content, Tag tag) {
+    this.content = content;
+    this.tag = tag;
+    this.id = new ContentTagId(content.getId(), tag.getId());
+  }
+}
