be1c9110ebfb112defec2424b4dc734b9759fda7
김현민 <rlagusals911@naver.com>
2026-06-29 10:50:06 +0900

    fix: 컴파일 오류 해결

--- src/main/java/com/example/sb10_MoPl_team3/auth/dto/response/JwtDto.java
+++ src/main/java/com/example/sb10_MoPl_team3/auth/dto/response/JwtDto.java
@@ -4,7 +4,6 @@ import com.example.sb10_MoPl_team3.user.dto.response.UserDto;
 
 public record JwtDto(
         UserDto userDto,
-        String accessToken,
-        String refreshToken
+        String accessToken
 ) {
 }
