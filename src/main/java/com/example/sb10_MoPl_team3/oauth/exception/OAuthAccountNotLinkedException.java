605b9f2fa9e89ebcc51d89439d5ed8990a2ce837
김현민 <rlagusals911@naver.com>
2026-07-14 16:13:26 +0900

    refactor: 미사용 예외 제거

--- src/main/java/com/example/sb10_MoPl_team3/oauth/exception/OAuthAccountNotLinkedException.java
+++
@@ -1,8 +0,0 @@
-package com.example.sb10_MoPl_team3.oauth.exception;
-
-public class OAuthAccountNotLinkedException extends RuntimeException {
-
-    public OAuthAccountNotLinkedException() {
-        super("OAuth account is not linked");
-    }
-}
